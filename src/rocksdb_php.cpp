62267f3f5642ffb4c11a845ededb079c7088d95d
Photonios <photonios@outlook.com>
2013-11-23 23:46:14 +0100

    Added some test code to see if the php extension would work.. yes :)

--- src/rocksdb_php.cpp
+++ src/rocksdb_php.cpp
@@ -1,10 +1,23 @@
 #include <phpcpp/phpcpp.h>
 
+extern Php::Value get_test_string();
+
+Php::Value get_test_string()
+{
+	return std::string("We are awesome!");
+}
+
+void add_extension_methods(Php::Extension &extension)
+{
+	extension.add("get_test_string", get_test_string);
+}
+
 extern "C"
 {
 	PHPCPP_EXPORT void* get_module()
 	{
 		static Php::Extension rocksdb_extension("RocksDB PHP Extension", "1.0");
+		add_extension_methods(rocksdb_extension);
 		return rocksdb_extension.module();
 	}
 }
---
+++ test.php
@@ -0,0 +1,5 @@
+#!/usr/bin/php -q
+
+<?php
+	echo get_test_string();
+?>
