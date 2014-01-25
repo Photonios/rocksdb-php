7a8c530bcfd7f33e23b9ccc100102750ddf351da
Photonios <photonios@outlook.com>
2014-01-26 00:59:22 +0100

    Revert to a2c088017a3117bf1a8165f31d1ad2bb487c11cd

--- src/includes.h
+++
@@ -1,7 +0,0 @@
-#ifndef ROCKSDB_INCLUDES_H_
-#define ROCKSDB_INCLUDES_H_
-
-#include <phpcpp/phpcpp.h>
-#include <rocksdb/db.h>
-
-#endif /* ROCKSDB_INCLUDES_H_ */
--- test_valgrind.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export USE_ZEND_ALLOC=0
-export ZEND_DONT_UNLOAD_MODULES=1
-valgrind --tool=memcheck --leak-check=full --num-callers=30 php test/test.php
-
